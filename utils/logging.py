29e010238c3a0773194c399caca1d107df72f6a9
Matej Arlović <matej.arlovic@gmail.com>
2022-03-24 09:55:09 +0100

    Implemented logging module.

---
+++ utils/logging.py
@@ -0,0 +1,18 @@
+import logging
+import logging.handlers
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Removes default logger handlers
+for handler in list(logger.handlers):
+    logger.removeHandler(handler)
+
+logfmt_str = "%(asctime)s %(levelname)-8s pid:%(process)d %(name)s:%(lineno)03d:%(funcName)s %(message)s"
+formatter = logging.Formatter(logfmt_str)
+
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(formatter)
+streamHandler.setLevel(logging.DEBUG)
+
+logger.addHandler(streamHandler)
