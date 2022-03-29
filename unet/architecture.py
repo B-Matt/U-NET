Fixed error with giving wrong shape on the output.

unet/architecture.py
@@ -8,7 +8,7 @@ class DoubleConv(nn.Module):
         Class that creates double convolution used inside U-Net architecture.
         (conv -> BN -> ReLU) * 2
     """
-    def __init__(self, in_channels, out_channels, padding=0):
+    def __init__(self, in_channels, out_channels, padding=1):
         super().__init__()
 
         self.double_conv = nn.Sequential(
@@ -32,7 +32,7 @@ class DownConv(nn.Module):
 
         self.max_pool = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_channels=in_channels, out_channels=out_channels)
+            DoubleConv(in_channels, out_channels)
         )
 
     def forward(self, x):
@@ -54,15 +54,15 @@ class UpConv(nn.Module):
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-
+        
+        print('sizes',x1.size(),x2.size(),diffX // 2, diffX - diffX//2, diffY // 2, diffY - diffY//2)
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
-        
         x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
+        x = self.conv(x)
+        return x
 
 class OutConv(nn.Module):
     """

unet/model.py
@@ -1,17 +1,14 @@
 from architecture import *
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False) -> None:
+    def __init__(self, n_channels, n_classes, bilinear=False):
         super(UNet, self).__init__()
-
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
 
         self.in_conv = DoubleConv(n_channels, 64)
         self.down_conv_1 = DownConv(64, 128)
         self.down_conv_2 = DownConv(128, 256)
         self.down_conv_3 = DownConv(256, 512)
+
         factor = 2 if bilinear else 1
         self.down_conv_4 = DownConv(512, 1024 // factor)
 
@@ -35,3 +32,14 @@ class UNet(nn.Module):
         x = self.up_conv_4(x, x1)
         x = self.out_conv(x)
         return self.last_activation(x)
+
+def test():
+    x = torch.randn((3, 1, 160, 160))
+    model = UNet(1, 1, False)
+    preds = model(x)
+
+    print(preds.shape, x.shape)
+    assert preds.shape == x.shape
+
+if __name__ == "__main__":
+    test()